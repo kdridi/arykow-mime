aec1c718fd7edac687b37407bd881ccd8aa92573
Karim DRIDI <karim.dridi@gmail.com>
2013-12-06 07:59:55 +0100

    parseFromBuffer and parseFromFile functions

--- test/arykow-mime_test.js
+++ test/arykow-mime_test.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var arykow_mime = require('../lib/arykow-mime.js');
+var arykow = {
+  mime: require('../lib/arykow-mime.js')
+};
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +24,85 @@ var arykow_mime = require('../lib/arykow-mime.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
+exports['parseFromBuffer'] = {
   setUp: function(done) {
     // setup here
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(arykow_mime.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+  'failureFromBufferNULL': function(test) {
+    arykow.mime.parseFromBuffer(null).then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(3);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.equal(error.message, arykow.mime.errors.NULL);
+      test.done();
+    });
+  },
+};
+
+exports['parseFromFile'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'failureFromFileFSERR': function(test) {
+    arykow.mime.parseFromFile('test/data/01.not.file').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(3);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.equal(error.message, arykow.mime.errors.FSERR);
+      test.done();
+    });
+  },
+  'failureFromFileINVALID': function(test) {
+    arykow.mime.parseFromFile('test/data/01.txt').then(function(result) {
+      test.expect(1);
+      test.equal(result, null);
+      test.done();
+    }, function(error) {
+      test.expect(3);
+      test.notEqual(error, null);
+      test.notEqual(error.message, null);
+      test.equal(error.message, arykow.mime.errors.INVALID);
+      test.done();
+    });
+  },
+  'successFromFileGIF': function(test) {
+    arykow.mime.parseFromFile('test/data/01.gif').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.name, null);
+      test.equal(result.name, 'image/gif');
+      test.notEqual(result.extension, null);
+      test.equal(result.extension, 'gif');
+      test.done();
+    }, function(error) {
+      test.expect(1);
+      test.equal(error, null);
+      test.done();
+    });
+  },
+  'successFromFilePNG': function(test) {
+    arykow.mime.parseFromFile('test/data/01.png').then(function(result) {
+      test.expect(5);
+      test.notEqual(result, null);
+      test.notEqual(result.name, null);
+      test.equal(result.name, 'image/png');
+      test.notEqual(result.extension, null);
+      test.equal(result.extension, 'png');
+      test.done();
+    }, function(error) {
+      test.expect(1);
+      test.equal(error, null);
+      test.done();
+    });
   },
 };
